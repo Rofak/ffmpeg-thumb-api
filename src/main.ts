3ed898c46f540bd3174c4b20cc682dd352a5798c
Rofak <rofakvkm@gmail.com>
2025-11-23 10:04:24 +0700

    chore: refactor

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
     origin: ['https://similartoolz.com', 'http://localhost:3000'],
-    methods: 'GET',
+    methods: ['GET', 'DELETE'],
     allowedHeaders: 'Content-Type, Authorization',
     credentials: true,
   });
