d13f94ab0f227bf250414f86be55ee05a5e70602
Rofak <rofakvkm@gmail.com>
2026-05-29 21:51:16 +0700

    chore: allow corrs

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: ['https://similartoolzai.com', 'http://localhost:3000'],
+    origin: ['https://similartoolz.net', 'http://localhost:3000'],
     methods: ['GET', 'DELETE'],
     allowedHeaders: 'Content-Type, Authorization',
     credentials: true,
